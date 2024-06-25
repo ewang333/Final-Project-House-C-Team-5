5b42d8d9cbda513eac1f35825312a940123203ac
Eileen Wang <eileen.wang915@gmail.com>
2024-06-25 13:09:26 -0400

    Initial Commit

---
+++ Final Project House C Team 5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Final Project House C Team 5
+//
+//  Created by Eileen Wang on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Final Project House C Team 5/Final_Project_House_C_Team_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_House_C_Team_5App.swift
+//  Final Project House C Team 5
+//
+//  Created by Eileen Wang on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_House_C_Team_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
