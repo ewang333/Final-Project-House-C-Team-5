74cfcf0aa51256350469dee13053926d2778d159
Eileen Wang <eileen.wang915@gmail.com>
2024-06-26 10:43:16 -0400

    Added second page of mobile app

---
+++ Final Project House C Team 5/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  Final Project House C Team 5
+//
+//  Created by Eileen Wang on 6/26/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("HOME")
+    }
+}
+
+#Preview {
+    Home()
+}
--- Final Project House C Team 5/SignUp.swift
+++ Final Project House C Team 5/SignUp.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct SignUp: View {
+    @State private var name = ""
+    @State private var dOB = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("SMART").font(.largeTitle).fontWeight(.bold).foregroundColor(.color3)
+                    .padding(.top, 50)
+                    .padding(.bottom, 20)
+                
+                Text("Get Started Today").font(.title3).foregroundColor(.color4).padding(.bottom, 100)
+                
+                Text("NAME").font(.title2).foregroundColor(.color4).padding(.bottom, 10)
+                
+                TextField("John Doe", text: $name).padding(.vertical, 15).multilineTextAlignment(.center)
+                    .font(.title3)
+                    .border(Color.black, width: 2)
+                    .padding(.bottom, 35)
+                    .padding(.horizontal, 20)
+                
+                Text("DATE OF BIRTH").font(.title2).foregroundColor(.color4).padding(.bottom, 10)
+                
+                TextField("01/01/2001", text: $dOB).padding(.vertical, 15).multilineTextAlignment(.center)
+                    .font(.title3)
+                    .border(Color.black, width: 2)
+                    .padding(.bottom, 35)
+                    .padding(.horizontal, 20)
+                
+                
+                
+                Spacer()
+                
+                NavigationLink(destination: Home()) {
+                    Text("Continue")
+                        .padding(.vertical, 20)
+                        .padding(.horizontal, 120)
+                        .background(Color.color3)
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .cornerRadius(10)
+                        .shadow(radius: 1)
+                    }
+            }
+        }
     }
 }
 
