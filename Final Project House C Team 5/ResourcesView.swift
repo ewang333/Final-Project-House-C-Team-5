5a42725b738eb6eac2ead81a325aeece698d6e7c
Susan Egan <susanmargaretegan@gmail.com>
2024-06-27 10:33:11 -0400

    changed links to be blue and underlined

--- Final Project House C Team 5/ResourcesView.swift
+++ Final Project House C Team 5/ResourcesView.swift
@@ -37,16 +37,20 @@ struct ResourcesView: View {
                     VStack{
                         Link(destination: URL(string: "https://www.heart.org/en/healthy-living/healthy-eating/eat-smart/nutrition-basics/aha-diet-and-lifestyle-recommendations")!) {
                             Text("American Health Association")
-                            .foregroundColor(Color.black)}
+                                .foregroundColor(Color.blue)
+                            .underline()}
+
                         
                         Link(destination: URL(string: "https://www.nutrition.gov")!) {
                             Text("Nutrition.gov")
-                            .foregroundColor(Color.black)}
+                                .foregroundColor(Color.blue)
+                            .underline()}
                         
                         
                         Link(destination: URL(string: "https://www.heart.org/en/healthy-living/healthy-eating/eat-smart/nutrition-basics/aha-diet-and-lifestyle-recommendations")!) {
                             Text("National Instutitues of Health")
-                            .foregroundColor(Color.black)}
+                                .foregroundColor(Color.blue)
+                            .underline()}
                         
                         Spacer()
                         
@@ -62,15 +66,19 @@ struct ResourcesView: View {
                             VStack{
                                 Link(destination: URL(string: "https://www.mayoclinichealthsystem.org/hometown-health/speaking-of-health/dont-fall-for-a-fad-diet")!) {
                                     Text("Mayo Clinic of Health")
-                                    .foregroundColor(Color.black)}
+                                        .foregroundColor(Color.blue)
+                                        .underline()
+                                }
                                 
                                 Link(destination: URL(string: "https://www.sciencedirect.com/science/article/pii/S2666149723000312")!) {
                                     Text("Affect on Oral Health")
-                                    .foregroundColor(Color.black)}
+                                        .underline()
+                                    .foregroundColor(Color.blue)}
                                 
                                 Link(destination: URL(string: "https://www.sciencedirect.com/science/article/pii/S2666149723000312")!) {
                                     Text("The Association of UK Dietitians")
-                                    .foregroundColor(Color.black)}
+                                        .underline()
+                                    .foregroundColor(Color.blue)}
                                 
                                 Spacer()
                                 
@@ -86,16 +94,19 @@ struct ResourcesView: View {
                                     VStack{
                                         Link(destination: URL(string: "https://www.who.int/news-room/fact-sheets/detail/physical-activity")!) {
                                             Text("World Health Organization")
-                                                .foregroundColor(Color.black)
+                                                .underline()
+                                                .foregroundColor(Color.blue)
                                             .lineLimit(nil)}
                                         
                                         Link(destination: URL(string: "https://niddk.nih.gov/health-information/weight-management/staying-active-at-any-size")!) {
                                             Text("National Institutes of Health")
-                                            .foregroundColor(Color.black)}
+                                                .underline()
+                                            .foregroundColor(Color.blue)}
                                         
                                         Link(destination: URL(string: "https://www.health.harvard.edu/topics/exercise-and-fitness")!) {
                                             Text("Harvard Health")
-                                            .foregroundColor(Color.black)}
+                                                .underline()
+                                            .foregroundColor(Color.blue)}
                                     }
                                 }
                                 Spacer()
@@ -112,15 +123,18 @@ struct ResourcesView: View {
                                     VStack{
                                         Link(destination: URL(string: "https://www.health.harvard.edu/blog/heart-rate-variability-new-way-track-well-2017112212789")!) {
                                             Text("Harvard Health")
-                                            .foregroundColor(Color.black)}
+                                                .underline()
+                                            .foregroundColor(Color.blue)}
                                         
                                         Link(destination: URL(string: "https://www.scientificamerican.com/article/how-to-keep-muscles-strong-as-you-age/")!) {
                                             Text("Scientific American")
-                                            .foregroundColor(Color.black)}
+                                                .underline()
+                                            .foregroundColor(Color.blue)}
                                         
                                         Link(destination: URL(string: "https://my.clevelandclinic.org/health/body/21196-immune-system")!) {
                                             Text("Cleaveland Clinic")
-                                            .foregroundColor(Color.black)}
+                                                .underline()
+                                            .foregroundColor(Color.blue)}
                                     }
                                 }
                                 VStack{
@@ -136,15 +150,18 @@ struct ResourcesView: View {
                                     VStack{
                                         Link(destination: URL(string: "https://www.nami.org/about-mental-illness/mental-health-conditions/")!) {
                                             Text("National Alliance on Mental Illness")
-                                            .foregroundColor(Color.black)}
+                                                .underline()
+                                            .foregroundColor(Color.blue)}
                                         
                                         Link(destination: URL(string: "https://www.who.int/news-room/fact-sheets/detail/mental-health-of-older-adults")!) {
                                             Text("World Health Association")
-                                            .foregroundColor(Color.black)}
+                                                .underline()
+                                            .foregroundColor(Color.blue)}
                                         
                                         Link(destination: URL(string: "https://www.hopkinsmedicine.org/health/wellness-and-prevention/what-are-common-symptoms-of-autoimmune-disease")!) {
                                             Text("Johns Hopkins Medicine")
-                                            .foregroundColor(Color.black)}
+                                                .underline()
+                                            .foregroundColor(Color.blue)}
                                     }
                                 }
                                 
